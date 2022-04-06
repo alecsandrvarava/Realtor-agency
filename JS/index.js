127e76f29a859233405248fce37bc1b0af4530c6
alecsandrvarava <alecsandrvarava@ukr.net>
2022-04-07 00:40:41 +0300

    first commit

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+// const hidden = document.querySelectorAll(".hidden")
+const menuBurger = document.querySelector('.menu__burger')
+const headerMenu = document.querySelector('.header-menu__list')
+const wrapper = document.querySelector('.wrapper')
+
+if (screen.width < 991) {
+	headerMenu.classList.add('opacity')
+} else {
+	headerMenu.classList.remove('opacity')
+}
+
+menuBurger.onclick = function () {
+	if (headerMenu.classList.contains('opacity')) {
+		console.log('!')
+		this.classList.add('animation')
+		headerMenu.classList.remove('opacity')
+		wrapper.classList.add('stop')
+	} else {
+		console.log('!!')
+		this.classList.remove('animation')
+		headerMenu.classList.add('opacity')
+		wrapper.classList.remove('stop')
+	}
+}
