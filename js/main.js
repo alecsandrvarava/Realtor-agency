2e0bd3bf446e57091644e4ba0efe4db35f97cc13
alecsandrvarava <alecsandrvarava@ukr.net>
2022-04-08 23:47:22 +0300

    main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-// const hidden = document.querySelectorAll(".hidden")
+const hidden = document.querySelectorAll(".hidden")
 const menuBurger = document.querySelector('.menu__burger')
 const headerMenu = document.querySelector('.header-menu__list')
-const wrapper = document.querySelector('.wrapper')
+const [popupLink] = document.querySelectorAll(".popup__link")
+const [popupClose] = document.querySelectorAll(".popup__close")
+const [popupArea] = document.querySelectorAll(".popup__area")
+const popup = document.querySelector('.popup')
+const body = document.querySelector('body')
 
 if (screen.width < 991) {
 	headerMenu.classList.add('opacity')
@@ -18,7 +22,7 @@ menuBurger.onclick = function () {
 		setTimeout(() => {
 			headerMenu.classList.remove('hidden')
 		}, 200)
-		wrapper.classList.add('stop')
+		body.classList.add('lock')
 	} else {
 		console.log('!!')
 		this.classList.remove('animation')
@@ -26,7 +30,23 @@ menuBurger.onclick = function () {
 		setTimeout(() => {
 			headerMenu.classList.add('hidden')
 		}, 200)
-		// headerMenu.classList.add('hidden')
-		wrapper.classList.remove('stop')
+		body.classList.remove('lock')
 	}
+}
+
+popupLink.onclick = function () {
+	popup.classList.add('open')
+	body.classList.add('lock')
+}
+popupClose.onclick = function () {
+	popup.classList.remove('open')
+	setTimeout(() => {
+		body.classList.remove('lock')
+	}, 300)
+}
+popupArea.onclick = function () {
+	setTimeout(() => {
+		body.classList.remove('lock')
+	}, 300)
+	popup.classList.remove('open')
 }
