0ab022fe457ec7695182ea1dcf43d5d1fff23d41
alecsandrvarava <alecsandrvarava@ukr.net>
2022-04-09 14:21:07 +0300

    main

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const headerMenu = document.querySelector('.header-menu__list')
 const [popupLink] = document.querySelectorAll(".popup__link")
 const [popupClose] = document.querySelectorAll(".popup__close")
 const [popupArea] = document.querySelectorAll(".popup__area")
+const [formSubmission] = document.querySelectorAll(".form__submission")
 const popup = document.querySelector('.popup')
 const body = document.querySelector('body')
 
@@ -22,7 +23,7 @@ menuBurger.onclick = function () {
 		setTimeout(() => {
 			headerMenu.classList.remove('hidden')
 		}, 200)
-		body.classList.add('lock')
+		body.classList.add('lock-padding')
 	} else {
 		console.log('!!')
 		this.classList.remove('animation')
@@ -30,23 +31,43 @@ menuBurger.onclick = function () {
 		setTimeout(() => {
 			headerMenu.classList.add('hidden')
 		}, 200)
-		body.classList.remove('lock')
+		body.classList.remove('lock-padding')
 	}
 }
 
 popupLink.onclick = function () {
 	popup.classList.add('open')
-	body.classList.add('lock')
+	body.classList.add('lock-padding')
+	menuBurger.classList.remove('animation')
+	headerMenu.classList.add('opacity')
+	setTimeout(() => {
+		headerMenu.classList.add('hidden')
+	}, 200)
+	// body.classList.remove('lock')
 }
 popupClose.onclick = function () {
 	popup.classList.remove('open')
 	setTimeout(() => {
-		body.classList.remove('lock')
+		body.classList.remove('lock-padding')
 	}, 300)
+	// headerMenu.classList.remove('opacity')
+	// setTimeout(() => {
+	// 	headerMenu.classList.remove('hidden')
+	// }, 200)
 }
 popupArea.onclick = function () {
 	setTimeout(() => {
-		body.classList.remove('lock')
+		body.classList.remove('lock-padding')
 	}, 300)
 	popup.classList.remove('open')
+	// headerMenu.classList.remove('opacity')
+	// setTimeout(() => {
+	// 	headerMenu.classList.remove('hidden')
+	// }, 200)
 }
+// formSubmission.onclick = function () {
+// 	popup.classList.remove('open')
+// 	setTimeout(() => {
+// 		body.classList.remove('lock')
+// 	}, 300)
+// }
